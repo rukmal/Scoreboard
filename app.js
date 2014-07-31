cf3b66053fa546b07d22821dcc1a52e3a827d5a2
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2014-07-31 13:52:19 +0530

    Mostly finished the landing page, and added more parameters to the application config file

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-// Setup basic express server
+/**
+ * Scoreboard server logic
+ *
+ * @author Rukmal Weerawarana
+ */
+
 var express = require('express');
 var app = express();
 var path = require('path');
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var fs = require('fs');
 var port = process.env.PORT || 3000;
 
+
+// Starting server
 server.listen(port, function () {
 	console.log('Scoreboard running on port ' + port);
 });
 
-// all environments
+
+// Express middleware
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -24,8 +33,14 @@ app.use(express.cookieParser('your secret here'));
 app.use(express.session());
 app.use(app.router);
 
+
+// Loading board configuration from config file
+var board_cfg = JSON.parse(fs.readFileSync('config.json'));
+
+
+// Handling routing
 app.get('/', function (req, res) {
-	res.render('index')
+	res.render('index');
 });
 
 app.get('/board', function (req, res) {
@@ -36,6 +51,8 @@ app.get('/remote', function (req, res) {
 	res.render('remote');
 });
 
+
+// 
 io.on('connection', function (socket) {
 	socket.on('test', function (data) {
 		console.log(data);
