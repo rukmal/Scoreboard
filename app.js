946b41febee7b6f69575d91b87c8666ef96c9cfd
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2014-07-31 11:41:18 +0530

    Added an Express 3 skeleton with socket.io

---
+++ app.js
@@ -0,0 +1,29 @@
+// Setup basic express server
+var express = require('express');
+var app = express();
+var path = require('path');
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
+
+server.listen(port, function () {
+	console.log('Scoreboard running on port ' + port);
+});
+
+// all environments
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+
+io.on('connection', function (socket) {
+
+});
